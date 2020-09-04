697fe93b53230c81ef3fbde4b3bb7aa444715533
lora96sh <57793171+lora96sh@users.noreply.github.com>
2020-09-04 14:02:52 +0300

    g

--- api/addCustomer.php
+++ api/addCustomer.php
@@ -1,26 +1,25 @@
-<!-- <?php require("../db.php");?>
-<?php 
-header('Content-Type: application/json');
-$email=$_POST['email'];
-$password=$_POST['password'];
+<!-- <?php require("../db.php");?> -->
+<!-- <?php  -->
+// header('Content-Type: application/json');
+// $email=$_POST['email'];
+// $password=$_POST['password'];
 
-$result = $conn->query("SELECT email FROM Customers WHERE email = '$email'");
-if($result->num_rows == 0) {
+// $result = $conn->query("SELECT email FROM Customers WHERE email = '$email'");
+// if($result->num_rows == 0) {
     
-    $sql = "INSERT INTO `Customers`( `email`, `password`)
-    VALUES ('$email','$password')";
+//     $sql = "INSERT INTO `Customers`( `email`, `password`)
+//     VALUES ('$email','$password')";
 
-    if ($conn->query($sql)===TRUE) {
-        echo json_encode($conn->insert_id);
-    }else {
-        echo json_encode($conn->error);
-    }
-} else {
-    echo json_encode($conn->error);
-}
-$mysqli->close();
+//     if ($conn->query($sql)===TRUE) {
+//         echo json_encode($conn->insert_id);
+//     }else {
+//         echo json_encode($conn->error);
+//     }
+// } else {
+//     echo json_encode($conn->error);
+// }
+// $mysqli->close();
 
 
 
-// end of the file
-$conn->close();
+// $conn->close();
--- listProducts.php
+++ listProducts.php
@@ -71,6 +71,26 @@
             </div>
         </div>
     </div>
+<!-- 
+    <div class="modal fade remove" tabindex="-1" role="dialog">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Are you sure?</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>You are about to delete </p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" @click="deleteProduct">Delete</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                </div>
+            </div>
+        </div>
+    </div> -->
 
         <br><br>
         <div class="footer">
